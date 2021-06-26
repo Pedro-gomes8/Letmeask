f3d074c2af3d06a584577bbc2f1927033875f715
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-06-25 21:27:09 -0300

    Project done, deploy done

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -8,11 +8,23 @@ type QuestionType = {
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHighLighted?: boolean;
 };
 
-export function Question({ content, author, children }: QuestionType) {
+export function Question({
+  content,
+  author,
+  children,
+  isAnswered = false,
+  isHighLighted = false,
+}: QuestionType) {
   return (
-    <div className="question">
+    <div
+      className={`question ${isAnswered ? "answered" : ""} ${
+        isHighLighted && !isAnswered ? "highlighted" : ""
+      }`}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -7,7 +7,8 @@ import { RoomCode } from "../components/RoomCode";
 
 import logoImg from "../assets/images/logo.svg";
 import deleteImg from "../assets/images/delete.svg";
-
+import checkImg from "../assets/images/check.svg";
+import answerImg from "../assets/images/answer.svg";
 // import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
 import "../styles/room.scss";
@@ -28,13 +29,23 @@ export function AdminRoom() {
     await database.ref(`rooms/${roomId}`).update({
       endedAt: new Date(),
     });
+    history.push("/");
   };
 
   const handleDeleteQuestion = async (questionId: string) => {
     if (window.confirm("Tem certeza que deseja excluir essa pergunta?")) {
       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
-    history.push("/");
+  };
+  const handleCheckQuestionAnswered = async (questionId: string) => {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  };
+  const handleHighlightQuestion = async (questionId: string) => {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighLighted: true,
+    });
   };
   return (
     <div id="page-room">
@@ -65,7 +76,28 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighLighted={question.isHighLighted}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAnswered(question.id)}
+                    >
+                      <img
+                        src={checkImg}
+                        alt="Marcar pergunta como respondida"
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque a pergunta" />
+                    </button>
+                  </>
+                )}
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
